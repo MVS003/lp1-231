e8ad60bde1bbbeba28f1553f2876648d6bc9fc15
Maysa <maysavieira003@gmail.com>
2023-03-07 17:54:43 -0300

    Introdução a java

---
+++ src/semana03/Introducao.java
@@ -0,0 +1,35 @@
+package semana03;
+
+public class Introducao {
+    public static void main(String[] args) {
+        //Comentario de uma linha
+        /*
+        *
+        *
+        */
+        
+        // Tipos primitivos
+        // Inteiros
+        //byte, short, int, long
+
+        // Ponto Flutuante
+        float nota1 = 10.34343f; //4 bytes
+        double nota2 = 10.434343; //8 bytes
+
+        //char
+        char letra = 'A';
+        
+        //boolean
+        boolean ligado = true;
+        boolean teste = false;
+
+        // String
+        String nome = "João da Silva";
+
+        System.out.println(nome);
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+
+        
+    }
+}
