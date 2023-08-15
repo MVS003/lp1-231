3a5188d88757c529c27656769fbd177d7ecf5a17
Maysa <maysavieira003@gmail.com>
2023-08-15 17:33:35 -0300

    semana19

---
+++ src/semana19/exercicios/Empresa.java
@@ -0,0 +1,12 @@
+package semana19.exercicios;
+
+public class Empresa {
+    private String cnpj;
+    private String nome;
+
+    public Empresa(String cnpj, String nome) {
+        this.cnpj = cnpj;
+        this.nome = nome;
+    }
+
+}
